03e788856e88c5482dd658dd2d898502d2b99526
Lck <kle995301@gmail.com>
2024-05-24 22:30:12 +0700

    dhhjdhjdjdjh

--- src/main/java/com/example/demo/AsmApplication.java
+++ src/main/java/com/example/demo/AsmApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AsmApplication {
 
 	public static void main(String[] args) {
+		System.out.println("shjsdhjs");
 		SpringApplication.run(AsmApplication.class, args);
 	}
 
